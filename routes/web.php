74a9485ff00446b5ddc01f68af8bf7d4130970ae
Maximiliano Giménez <maxxi02gimenez@gmail.com>
2025-03-17 09:37:43 -0300

    v 0.101

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,6 @@ Route::get('/', function () {
 
 Route::view('/about','about');
 
-Route::get('/product/{category?}',function(string $category = null){
-    return 1;
-}){};
 
 /*
 Route::get('/about', function () {

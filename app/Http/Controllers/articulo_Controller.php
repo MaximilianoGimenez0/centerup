53e3fb2046b776b61d03764fd8821faa2558c8a7
Maximiliano Giménez <maxxi02gimenez@gmail.com>
2025-03-17 11:29:26 -0300

    v 0.103

---
+++ app/Http/Controllers/articulo_Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class articulo_Controller extends Controller
+{
+    public function index(){
+        return "Ola";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,51 @@
 <?php
 
+use App\Http\Controllers\articulo_Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    //LLAMANDO A UN RUTA POR SU NOMBRE ASOCIADO
+    $url = route('about');
+    dd($url);
+
     return view('welcome');
 });
 
-Route::view('/about','about');
+//CON ->name SE PUEDE ASOCIAR UN NOMBRE A UNA RUTA
+Route::view('/about','about')->name('about');
 
+Route::get('/articulo/{categoria?}/{id?}',function(string $categoria = null, string $id = null){
+    return "Categoría = $categoria | id = $id";
+})->name('articulo');
+
+Route::fallback(function(){
+    return 'ERROR 404 O.O';
+});
+
+Route::get('/sum/{first_num?}/{second_num?}',function(float $first_num = null,float $second_num = null){
+    return $first_num + $second_num;
+})->whereNumber(['first_num','second_num']);
+
+
+Route::get('/test',[articulo_Controller::class,'index']);
+
+/*
 Route::get('/products/{category?}',function(string $category=null){
     return "Product for category= $category";
 });
 
-/*
+
+//ONLY WORKS IF IT'S A NUMBER! 
+Route::get('/product/{id?}',function($id = null){
+    return "Product for category= $id";
+})->whereNumber('id');
+
+//SOLAMENTE INCLUYE ALGUNOS VALORES POSIBLES
+Route::get('/user/{name?}',function($name = null){
+    return "Nombre del usuario = $name";
+})->whereIn('name',['jorge','juan']);
+
 Route::get('/about', function () {
         return view('about');
 });

afbb0b913b156f1140603d02677946a844654652
Maximiliano Giménez <maxxi02gimenez@gmail.com>
2025-03-18 17:27:59 -0300

    cambios en las rutas

---
+++ app/Http/Controllers/homeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class homeController
+{
+public function index(){
+    return 'INDEX';
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 
 
 use App\Http\Controllers\articuloController;
+use App\Http\Controllers\homeController;
 use App\Http\Controllers\mathController;
 use Illuminate\Support\Facades\Route;
 
 Route::view(uri: '/about',view: 'about')->name(name: 'about ');
 
-Route::get('/sum/{num1}/{num2}',[mathController::class,'sum'])->whereNumber(['a','b']);
-Route::get('/subtract/{num1}/{num2}',[mathController::class,'subtract'])->whereNumber(['a','b']);
+Route::get('/sum/{a}/{b}',[mathController::class,'sum'])->whereNumber(['a','b']);
+Route::get('/subtract/{a}/{b}',[mathController::class,'subtract'])->whereNumber(['a','b']);
+
+Route::get('/',[homeController::class,'index'])->name('home');
